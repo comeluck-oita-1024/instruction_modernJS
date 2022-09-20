00aadb3677241dd118d6dded4525caf0ba3de211
comeluck-oita-1024 <112835235+comeluck-oita-1024@users.noreply.github.com>
2022-09-20 14:34:46 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// const val4 = {
+//   name: "kapa",
+//   age: 23
+// };
+
+// console.log(val4);
+
+// val4.name = "jak";
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// console.log(val5);
+
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
